41cdf57f830d90d8cdd367c10390b50164291bcf
pirluoxue <cbq850106918@163.com>
2019-03-21 15:25:03 +0800

    resttemplate免异常过滤

--- src/main/java/com/example/demo/configuration/RestTemplateConfig.java
+++ src/main/java/com/example/demo/configuration/RestTemplateConfig.java
@@ -3,16 +3,20 @@ package com.example.demo.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 /**
- * RestTemplate配置
- * Created by shuye on 2017/6/7.
- */
+ * @Author chen_bq
+ * @Description RestTemplate配置
+ * @Date 2019/3/20 14:59
+ **/
 @Configuration
 public class RestTemplateConfig {
     @Bean
@@ -28,6 +32,19 @@ public class RestTemplateConfig {
         RestTemplate restTemplate = new RestTemplate(factory);
         restTemplate.getMessageConverters().set(1, new StringHttpMessageConverter(StandardCharsets.UTF_8));
 //        restTemplate.getMessageConverters().set(2, new ByteArrayHttpMessageConverter());
+        ResponseErrorHandler responseErrorHandler = new ResponseErrorHandler() {
+            @Override
+            public boolean hasError(ClientHttpResponse clientHttpResponse) throws IOException {
+                //不检查状态码及抛异常
+                return false;
+            }
+            @Override
+            public void handleError(ClientHttpResponse clientHttpResponse) throws IOException {
+
+            }
+        };
+        restTemplate.setErrorHandler(responseErrorHandler);
         return restTemplate;
     }
+
 }
